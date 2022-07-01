102ddef47255f1b0207fc2e0fbf78eaddf3cc4fb
Dell Schulz <dells@DESKTOP-P01LTV4.localdomain>
2022-07-01 10:52:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+let conte = 10
+while(conte >= 1){
+    console.log(conte);
+    conte --;
+}
+
+for(let impares = 1; impares <=100; impares +=2){
+    console.log(impares)
+}
+
+let pares = "0"
+while (pares <= 100){
+    console.log(pares)
+    pares +=2;
+}
